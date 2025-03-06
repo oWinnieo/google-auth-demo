81f9276b89a7518b57060a8dc575329e644a65d7
oWinnieo <48428659+oWinnieo@users.noreply.github.com>
2025-03-06 22:38:10 +0800

    Submit 250306 10.38pm testing auth

---
+++ src/app/home/page.js
@@ -0,0 +1,37 @@
+'use client'
+import Image from "next/image";
+// import { Greet } from '../components/greet'
+// import { Counter } from '../components/counter'
+import Link from 'next/link'
+// import Hello from '@components/hello'; // wtest ts
+// import { Auth } from '@components/auth/auth'
+import './style.scss'
+
+// export const Home = () => { // wtest wrong?
+const Home = () => {
+    return (
+    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="page-home">
+    {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"> */}
+        <div className="area area-welcome">
+            <div className="area-in">
+                <h2>Welcome to Winnie&apos;s World 1st Page</h2>
+                {/* <Hello name="TypeScript"></Hello> */}
+            </div>
+        </div>
+        <div className="area area-navigation">
+            {/* <div className="area-in">
+                <li><Link href="/about">About Me</Link></li>
+                <li><Link href="learning">Learning</Link></li>
+                
+            </div>
+            <Auth></Auth> */}
+        </div>
+        {/* <li><Link href="/blog/first-post">blog/first-post</Link></li> */}
+        {/* <Greet />
+        <Counter /> */}
+        {/* </main> */}
+    </div>
+    )
+}
+export default Home;
--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,32 @@
-"use client"; // 如果是 app router 需要这个
+// "use client"; // 如果是 app router 需要这个
 
-import { useSession, signIn, signOut } from "next-auth/react";
+// import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function Home() {
-  const { data: session } = useSession();
+// export default function Home() {
+//   const { data: session } = useSession();
 
+//   return (
+//     <div className="flex flex-col items-center mt-20">
+//       <p>session: {session && JSON.stringify(session)}</p>
+//       {session ? (
+//         <>
+//           <p>Welcome 123, {session.user.name}!</p>
+//           <img src={session.user.image} alt="User Avatar" className="w-16 h-16 rounded-full mt-2" />
+//           <button onClick={() => signOut()} className="mt-4 bg-red-500 text-white px-4 py-2 rounded">
+//             Sign out
+//           </button>
+//         </>
+//       ) : (
+//         <button onClick={() => signIn("google")} className="bg-blue-500 text-white px-4 py-2 rounded">
+//           Sign in with Google
+//         </button>
+//       )}
+//     </div>
+//   );
+// }
+import Home from './home/page'
+export default function Wrap() {
   return (
-    <div className="flex flex-col items-center mt-20">
-      <p>session: {session && JSON.stringify(session)}</p>
-      {session ? (
-        <>
-          <p>Welcome 123, {session.user.name}!</p>
-          <img src={session.user.image} alt="User Avatar" className="w-16 h-16 rounded-full mt-2" />
-          <button onClick={() => signOut()} className="mt-4 bg-red-500 text-white px-4 py-2 rounded">
-            Sign out
-          </button>
-        </>
-      ) : (
-        <button onClick={() => signIn("google")} className="bg-blue-500 text-white px-4 py-2 rounded">
-          Sign in with Google
-        </button>
-      )}
-    </div>
+    <Home />
   );
 }
